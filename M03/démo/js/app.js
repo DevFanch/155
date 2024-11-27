881cb02a64d63c2a0f853c31fd35842547caae3a
Devfanch <devfanch44@gmail.com>
2024-11-27 16:58:09 +0100

    Update Démo M03

---
+++ M03/démo/js/app.js
@@ -0,0 +1,13 @@
+const $sec = document.getElementById('sec1')
+console.log($sec);
+
+const $article = document.querySelector('article')
+console.log($article);
+
+$article.innerText = '<span>Replace</span>'
+console.log($article.id);
+
+// $article.innerHTML = '<span>Replace</span>'
+
+const $posts = document.querySelectorAll('article')
+console.log($posts);
