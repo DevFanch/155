a1c908650761aaac351fb3ef84a6af6b5059277d
Devfanch <devfanch44@gmail.com>
2024-11-28 10:13:22 +0100

    Démo M03

--- M03/démo/js/app.js
+++ M03/démo/js/app.js
@@ -5,9 +5,28 @@ const $article = document.querySelector('article')
 console.log($article);
 
 $article.innerText = '<span>Replace</span>'
+// $article.innerHTML = '<span>Replace</span>'
 console.log($article.id);
 
-// $article.innerHTML = '<span>Replace</span>'
 
-const $posts = document.querySelectorAll('article')
-console.log($posts);
+const $contentPosts = document.querySelectorAll('article p')
+console.log($contentPosts);
+
+for (const paraph of $contentPosts) {
+    // console.log(paraph.textContent);
+    // Add Event
+    // Function tradi
+    paraph.addEventListener('mouseenter', function(event) {
+        console.log(this); // La balise qui q déclenchée l'event
+        this.innerText = "J'ai changé le contenu"
+        console.log(event);
+    })
+    // Function =>
+    // paraph.addEventListener('mouseenter', (e) => {
+        // console.log(this); // window
+        // console.log(e.currentTarget)
+    // })
+}
+
+const $input = document.getElementById('nom')
+$input.addEventListener('input', e => console.log(e.currentTarget.value))
