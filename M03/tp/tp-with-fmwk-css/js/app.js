01de04626a0c6a7c127e91aa8ae1e803fc882122
Devfanch <devfanch44@gmail.com>
2024-11-28 16:51:44 +0100

    UPD : Tips Form submit

---
+++ M03/tp/tp-with-fmwk-css/js/app.js
@@ -0,0 +1,16 @@
+const $form = document.getElementById('user-contact')
+$form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    console.log(this.querySelectorAll('input'));
+    // Faire une boucle sur la collection
+
+        // Sur chaque element => test 
+        // si el.id === 'email' alors
+            // je test
+        // sinon
+
+    // Fin de la boucle
+
+    // Test est-ce que j'ai rencontré des erreurs ??
+    // Sinon j'enregistre
+})
